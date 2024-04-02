a3f7cf7262890f2874736e5edc2c8bc05c8b0578
Guilherme Sousa <123672664+uguisousa@users.noreply.github.com>
2024-04-02 18:34:56 -0300

    Add files via upload

---
+++ treino.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int idade;
+    
+    printf("Digite sua idade");
+    scanf("%d", &idade);
+    
+    if(idade => 18){
+      printf("Voce e de maior");         
+    }
+    else{
+      printf("Voce e de menor");     
+    }
+    
+    return 0; 
+}
